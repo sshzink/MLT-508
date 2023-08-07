47b98f00bec2985f5a2c4969c4c87a8f8b2dcbb8
sshzink <azink@arizona.edu>
2023-08-07 01:56:06 -0500

    Initial commit

--- app/services.py
+++ app/services.py
@@ -8,19 +8,16 @@ class Services:
         self.repo = MySQLRepository()
 
     def translate_word(self, word: str, target_language: LANG):
-        # Fetch word data for the input word
         word_data = self.repo.get_word_by_form(word, target_language)
         if not word_data:
             return None
 
         pos = POS(word_data.pos).name
 
-        # Determine the target column based on the target language
         target_column = f'eq_in_{target_language.value}'
         translation_id = getattr(word_data, target_column)
 
         if translation_id:
-            # Fetch the translation in the target language
             translation = self.repo.get_word_by_id(translation_id, target_language)
             if translation:
                 return f"{translation.form}, {pos}"
