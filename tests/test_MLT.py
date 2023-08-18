058eec4e9d3508bac758bffd6ce6b23be32bc721
sshzink <azink@arizona.edu>
2023-08-17 21:06:07 -0500

    final touches

--- tests/test_MLT.py
+++ tests/test_MLT.py
@@ -6,12 +6,9 @@ from model.enums import *
 def services():
     return Services()
 def test_translate_word(services):
-    # Assuming the word "cat" is already in the database with its translations
 
-    # Test translation from English to Spanish
     translation_es = services.translate_word("cat", LANG.ENGLISH, LANG.SPANISH)
     assert translation_es == "gato, NOUN"
 
-    # Test translation from English to French
     translation_fr = services.translate_word("cat", LANG.ENGLISH, LANG.FRENCH)
     assert translation_fr == "chat, NOUN"
